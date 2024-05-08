0be1a97d7ae1913a07a2207ef22b13c1731c926a
yelyzaveta.korolevych <korolevich.liza@gmail.com>
2024-05-08 22:11:45 +0300

    Implemendted authentication on the PasteBin website and action creation new paste with appropriate reqires

---
+++ src/tests/task1.tests.js
@@ -0,0 +1,20 @@
+describe('Task 1', () => {
+    before(async() => {
+        await browser.url("https://pastebin.com")
+        await $('a.sign-in').click()
+        await $('input#loginform-username').setValue('Epam-user')
+        await $('input#loginform-password').setValue('epam_test_12345')
+        await $('button.btn').click()
+    })
+    
+    it('creating new paste', async () => { 
+        await $('a.header__btn').click()
+        await $('textarea#postform-text').setValue('Hello from WebDriver')
+        await $('span#select2-postform-expiration-container').click()
+        await $('//li[contains(text(),"10 Minutes")]').click()
+        await $('input#postform-name').setValue('helloweb')
+        await $('//button[text()="Create New Paste"]').click()
+    
+        expect(await $('div.info-top h1').getText()).toEqual('helloweb')
+    })
+})
